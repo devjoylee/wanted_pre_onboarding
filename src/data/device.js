001923ada43e51b4c7efdf84266b0e039d761e4b
Joy Lee <joylee.dev@gmail.com>
2022-01-19 03:17:14 +0900

    반응형 구현 : 커스텀 미디어 쿼리 추가

---
+++ src/data/device.js
@@ -0,0 +1,13 @@
+const size = {
+  mobile: '767px',
+  tabletS: '991px',
+  tabletL: '1023px',
+  laptop: '1200px',
+};
+
+export const device = {
+  mobile: `(max-width: ${size.mobile})`,
+  tabletS: `(max-width: ${size.tabletS})`,
+  tabletL: `(max-width: ${size.tabletL})`,
+  laptop: `(max-width: ${size.laptop})`,
+};
